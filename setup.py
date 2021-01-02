3376be187a406828b93a5ca837d421911b74d3f6
Basil Huber <basil.huber@gmail.com>
2021-01-02 15:35:33 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='redlooper',
+      version='0.1',
+      description='Raspberry Pi Looper Pedal',
+      author='Basil',
+      author_email='basil.huber@gmail.com',
+      packages=['redlooper'],
+      install_requires=['cython', 'pyliblo', 'JACK-Client', 'RPi.GPIO'],
+      entry_points={'console_scripts': ['redlooper=redlooper.main:main']},
+      zip_safe=False)
