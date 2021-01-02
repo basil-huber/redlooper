994f200eca9cf1e6e56f9d355bef2639a49b5281
Basil Huber <basil.huber@gmail.com>
2021-01-02 23:16:51 +0100

    Added autostart
    Added redlooper.service
    install.sh: added pip install and installation of redlooper.service

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@ sudo apt-get install sooperlooper
 sudo apt-get install liblo-dev
 sudo apt install libffi-dev
 sudo apt-get install jackd
+sudo pip3 install -U git+https://github.com/basil-huber/redlooper.git#egg=redlooper
+
+# autostart
+mkdir -p ~/.config/systemd/user
+cp redlooper.service ~/.config/systemd/user
+systemctl --user enable redlooper
